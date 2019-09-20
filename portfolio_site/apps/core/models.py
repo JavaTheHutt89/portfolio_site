09b4c6591eb24b00a8d5c9df4daf7e8a634b5dbe
Юровник Максим Васильевич <21481@7733.ru>
2019-09-20 14:13:20 +1000

    added static file made changes in templates

--- portfolio_site/apps/core/models.py
+++ portfolio_site/apps/core/models.py
@@ -10,7 +10,8 @@ class Info(models.Model):
     website_link = models.CharField('Website', max_length=50)
     email = models.CharField("E-mail", max_length=75)
     phone_number = models.CharField("Номер телефона", max_length=20)
-    profile_image = models.ImageField(upload_to='media/pictures', blank=True)
+    profile_image = models.ImageField(upload_to=b'pictures',
+                                      blank=True, null=True)
 
     def __str__(self):
         return self.name + ' ' + self.position
